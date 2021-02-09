52767dfdf9a2b6c95b693a9e33fc872c7bf0de02
drawcall <ajiemath@gmail.com>
2021-02-09 14:32:22 +0800

    feat: remove old file

--- source/src/service/convert/flutter/transform/camel.js
+++
@@ -1,8 +0,0 @@
-const toCamel = val => {
-  let re = /-(\w)/g;
-  return val.replace(re, function ($0, $1) {
-    return $1.toUpperCase();
-  });
-};
-
-export default toCamel;
--- source/src/service/convert/flutter/transform/intercept.js
+++
@@ -1,7 +0,0 @@
-const intercept = (val, dot) => {
-  val += "";
-  val = val.split(" " || dot);
-  return val[0];
-};
-
-export default intercept;
